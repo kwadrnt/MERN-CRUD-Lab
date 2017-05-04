d581a3e7a63342e52173e9925bfe36f660c5b383
sejinpark0209 <sejinpark0209@gmail.com>
2017-05-04 12:22:25 -0700

    added DBUSER and DBPASS to server.js and postman works

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ var port = process.env.API_PORT || 3001;
 
 //db config
 //ADD YOUR INFO HERE!
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds123351.mlab.com:23351/mern-comment-box');
+var dbUser = process.env.MLAB_DBUSER
+var dbPassword = process.env.MLAB_DBPASSWORD
+var databaseUrl = 'mongodb://' + dbUser + ':' + dbPassword + '@ds013486.mlab.com:13486/mern-comment-box'
+mongoose.connect(databaseUrl)
 
 //config API to use bodyParser and look for JSON in req.body
 app.use(bodyParser.urlencoded({extended: true }));
@@ -45,7 +48,32 @@ router.route('/nuke').get(function(req,res){
   });
 });
 
-//add /comments route to our /api router here
+//adding the /comments route to our /api router
+router.route('/comments')
+  //retrieve all comments from the database
+  .get(function(req, res) {
+    //looks at our Comment Schema
+    Comment.find(function(err, comments) {
+      if (err)
+        res.send(err);
+      //responds with a json object of our database comments.
+      res.json(comments)
+    });
+  })
+  //post new comment to the database
+  .post(function(req, res) {
+    var comment = new Comment();
+    //body parser lets us use the req.body
+    comment.author = req.body.author;
+    comment.text = req.body.text;
+
+    comment.save(function(err) {
+      if (err)
+        res.send(err);
+      res.json({ message: 'Comment successfully added!' });
+    });
+  });
+
 
 //use router config when we call /API
 app.use('/api', router);
