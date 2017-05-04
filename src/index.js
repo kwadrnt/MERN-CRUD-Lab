53b4397be1463773d38ed7bfacfa5bb0d04cb253
sejinpark0209 <sejinpark0209@gmail.com>
2017-05-04 10:58:55 -0700

    sprint 1 setup components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+// main React File
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentBox from './CommentBox';
 
 //more React code here!
+ReactDOM.render(
+
+  document.getElementById('root')
+);
