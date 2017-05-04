187235377f44cb88a5540aef337fbc22170d8b07
sejinpark0209 <sejinpark0209@gmail.com>
2017-05-04 11:35:58 -0700

    changed server.js from heroku to mern-comment

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var port = process.env.API_PORT || 3001;
 
 //db config
 //ADD YOUR INFO HERE!
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds123351.mlab.com:23351/heroku_t96306zv');
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds123351.mlab.com:23351/mern-comment-box');
 
 //config API to use bodyParser and look for JSON in req.body
 app.use(bodyParser.urlencoded({extended: true }));
